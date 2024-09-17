955f83d2f2091540a7bfee07b10acb00ba7c5d55
ConnorAndry <139146840+ConnorAndry@users.noreply.github.com>
2024-09-17 13:37:02 -0700

    Finished weapon attack code.

--- Unity-Point-of-refrence/Assets/Scrips/ScripPlayerController.cs
+++ Unity-Point-of-refrence/Assets/Scrips/ScripPlayerController.cs
@@ -18,15 +18,23 @@ public class ScripPlayerController : MonoBehaviour
     public int healthRestore = 1;
 
     [Header("Weapon Stats")]
+    public int weaponID = 0;
+    public int fireMode = 0;
+    public float fireRate = 0;
+    public float currentClip = 0;
+    public float clipSize = 0;
+    public float maxAmmo = 0;
+    public float currentAmmo = 0;
+    public float reloadAmt = 0;
     public bool canFire = true;
 
-    public bool sprintMode = false;
 
     [Header("Movement Settings")]
     public float speed = 10.0f;
     public float jumpHeight = 5.0f;
     public float SprintMultplyer = 2.5f;
     public float groundDetectDistance = 1.0f;
+    public bool sprintMode = false;
 
     [Header("User Settings")]
     public bool sprintToggleOption = false;
@@ -56,6 +64,16 @@ public class ScripPlayerController : MonoBehaviour
 
         playercam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
         transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
+
+        if (Input.GetMouseButtonDown(0) && canFire && currentClip > 0)
+        {
+            canFire = false;
+            currentClip--;
+            StartCoroutine("cooldownFire");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+            reloadClip();
 
         Vector3 temp = myRB.velocity;
 
@@ -96,6 +114,36 @@ public class ScripPlayerController : MonoBehaviour
 
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Weapon")
+        {
+            other.gameObject.transform.position = WeaponSlot.position;
+
+            other.gameObject.transform.SetParent(WeaponSlot);
+
+            switch (other.gameObject.name)
+            {
+                case "Weapon 1":
+                    weaponID = 0;
+                    fireMode = 0;
+                    fireRate = 0.25f;
+                    currentClip = 20;
+                    clipSize = 20;
+                    maxAmmo = 400;
+                    currentAmmo = 200;
+                    reloadAmt = 20;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+       
+
+
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if ((health < maxHealth) && collision.gameObject.tag == "Health Pickup")
@@ -108,14 +156,51 @@ public class ScripPlayerController : MonoBehaviour
             Destroy(collision.gameObject);
         }
 
-        if (collision.gameObject.tag == "weapon")
-            collision.gameObject.transform.SetParent(WeaponSlot);
+       
+
+        if ((currentAmmo < maxAmmo) && collision.gameObject.tag == "AmmoPickup")
+        {
+            currentAmmo += reloadAmt;
+
+            if (currentAmmo > maxAmmo)
+                currentAmmo = maxAmmo;
+
+            Destroy(collision.gameObject);
+        }
+
+        
     }
 
-
-    IEnumerator cooldown(float time)
+    public void reloadClip()
     {
-        yield return new WaitForSeconds(time);
+        if (currentClip >= clipSize)
+            return;
+
+        else
+        {
+            float reloadCount = clipSize - currentClip;
+
+            if (currentAmmo < reloadCount)
+            {
+                currentClip += currentAmmo;
+
+                currentAmmo = 0;
+                return;
+            }
+
+            else
+            {
+                currentClip += reloadCount;
+
+                currentAmmo -= reloadCount;
+                return;
+            }
+        }
+    }
+
+    IEnumerator cooldownFire()
+    {
+        yield return new WaitForSeconds(fireRate);
         canFire = true;
 
     }
